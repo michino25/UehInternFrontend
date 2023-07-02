3aa747fbc80154700453422c8e81ddc550c41155
Tran Hoai Phuong <96610554+Coder20042002@users.noreply.github.com>
2023-07-03 05:21:14 +0700

    upload file

--- Utilities/Model.cs
+++ Utilities/Model.cs
@@ -158,7 +158,26 @@ namespace UehInternFrontend
     }
 
     
+    public class DangkyModel
+    {
+        public string mssv { get; set; }
+        public string ho { get; set; }
+        public string ten { get; set; }
+        public string lop { get; set; }
+        public string email { get; set; }
+        public string magv { get; set; }
+        public string makhoa { get; set; }
+        public string status { get; set; }
+    }
 
+     public class KetquaViewModed
+    {
+        public string TenSinhVien { get; set; }
+        public string MaSinhVien { get; set; }
+        public string Lop { get; set; }
+        public string Khoa { get; set; }
+        public double Diem { get; set; }
+    }
 
   
 
